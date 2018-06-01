17827ba75dd527034c5f731aeaf9a52557b9a276
Fabian Ellenberger <fabianellenberger@gmail.com>
2018-06-01 11:03:35 +0200

    [Update] add back button and show second expanded heading

--- components/Competencies/Competencies.vue
+++ components/Competencies/Competencies.vue
@@ -4,9 +4,10 @@
         h2.competencies__heading.expanded(v-if="isExpanded") {{headingExpanded}}
       div.competencies__wrap(:class="{ expanded: isExpanded }")
         transition(name="u-anim-fade-delayed")
-          h2.competencies__heading(v-if="!isExpanded") {{headingSimple}}
         .competencies__cards(:class="{ expanded: isExpanded }")
+          h2.competencies__heading(v-if="!isExpanded") {{headingSimple}}
           .competencies__card-wrap(v-for="area, index in competencies" :key="index" :class="[isExpanded ? 'expanded' : '', isExpanded && clickedCard !== index ? 'hide' : 'full-width']")
+            .competencies__back(v-show="isExpanded" @click="isExpanded ? handleExpand(index) : null") Back
             .competencies__card(@click="handleExpand(index)" :ref="'card-' + index")
               h3.competencies__card-title {{area.fields.title}}
               transition(name="u-anim-fade-delayed")
